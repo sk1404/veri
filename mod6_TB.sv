3a907130902669993ab028b7824b1d1d8305efde
sk1404 <sheshadri1404@gmail.com>
2025-07-29 13:08:12 +0530

    mod6_TB.sv

--- mod6_TB.sv
+++ mod6_TB.sv
@@ -1,17 +1,17 @@
-module test(c_inter.test1 t1);
-initial begin
-t1.dir<=0;
-#10 t1.reset<=0;
-#10 t1.reset<=1;
-#10 t1.reset<=0;
-#150 t1.dir<=1;
-#10 t1.reset<=0;
-#10 t1.reset<=1;
-#10 t1.reset<=0;
-#150 t1.dir<=0;
-#30 t1.reset<=0;
-#10 t1.reset<=1;
-#10 t1.reset<=0;
-#100 $finish;
+module test(c_inter.test1 t1); 
+initial begin 
+t1.dir<=0; 
+#10 t1.reset<=0; 
+#10 t1.reset<=1; 
+#10 t1.reset<=0; 
+#150 t1.dir<=1; 
+#30 t1.reset<=0; 
+#10 t1.reset<=1; 
+#10 t1.reset<=0; 
+#150 t1.dir<=0; 
+#30 t1.reset<=0; 
+#10 t1.reset<=1; 
+#10 t1.reset<=0; 
+#100 $finish; 
 end 
 endmodule
